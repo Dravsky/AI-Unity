b92a3ad219394869fc6c6bf4212c4ace722c15af
Dravsky <129130564+Dravsky@users.noreply.github.com>
2025-01-23 15:31:00 -0700

    Class (1/23)

--- Assets/Scripts/AIAutonomousAgent.cs
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 
 public class AIAutonomousAgent : AIAgent
 {
-    [Header("Wander")]
-    [SerializeField] float displacement;
-    [SerializeField] float distance;
-    [SerializeField] float radius;
+    [SerializeField] private AutonomousAgentData data;
 
     [Header("Perception")]
     public Perception seekPerception;
     public Perception fleePerception;
+    public Perception flockPerception;
 
     float angle;
 
@@ -44,23 +42,63 @@ public class AIAutonomousAgent : AIAgent
             }
         }
 
-        // WANDER
+        //WANDER
         if (movement.Acceleration.sqrMagnitude == 0)
         {
-            //randomly adjust angle +/- displacement
-            angle += Random.Range(-displacement, displacement);
-            //create rotation quaternion around y axis (up)
-            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
-            Vector3 point = rotation * (Vector3.forward * radius);
-            Vector3 forward = movement.Direction * distance;
-            Vector3 force = GetSteeringForce(forward + point);
-            movement.ApplyForce(force);
+            movement.ApplyForce(Wander());
+        }
+
+        //FLOCK
+        if (flockPerception != null)
+        {
+            var gameObjects = flockPerception.GetGameObjects();
+            if (gameObjects.Length > 0)
+            {
+                movement.ApplyForce(Cohesion(gameObjects) * data.cohesionWeight);
+                movement.ApplyForce(Separation(gameObjects, data.separationRadius) * data.cohesionWeight);
+                movement.ApplyForce(Alignment(gameObjects) * data.cohesionWeight);
+            }
+        }
+
+        Vector3 acceleration = movement.Acceleration;
+        acceleration.y = 0;
+        movement.Acceleration = acceleration;
+
+        if (movement.Direction.sqrMagnitude != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(movement.Direction);
         }
 
         //foreach (var go in gameObjects)
         //{
         //    Debug.DrawLine(transform.position, go.transform.position, Color.red);
         //}
+    }
+
+    private Vector3 Cohesion(GameObject[] neighbors)
+    {
+        Vector3 positions = Vector3.zero;
+        foreach (var neighbor in neighbors)
+        {
+            positions += neighbor.transform.position;
+        }
+
+        Vector3 center = positions / neighbors.Length;
+        Vector3 direction = center - transform.position;
+
+        Vector3 force = GetSteeringForce(direction);
+
+        return force;
+    }
+
+    private Vector3 Separation(GameObject[] neighbors, float radius)
+    {
+        return Vector3.zero;
+    }
+
+    private Vector3 Alignment(GameObject[] neighbors)
+    {
+        return Vector3.zero;
     }
 
     private Vector3 Seek(GameObject go)
@@ -79,11 +117,23 @@ public class AIAutonomousAgent : AIAgent
         return force;
     }
 
+    private Vector3 Wander()
+    {
+            //randomly adjust angle +/- displacement
+            angle += Random.Range(-data.displacement, data.displacement);
+            //create rotation quaternion around y axis (up)
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+            Vector3 point = rotation * (Vector3.forward * data.radius);
+            Vector3 forward = movement.Direction * data.distance;
+            Vector3 force = GetSteeringForce(forward + point);
+            return force;
+    }
+
     private Vector3 GetSteeringForce(Vector3 direction)
     {
-        Vector3 desired = direction.normalized * movement.maxSpeed;
+        Vector3 desired = direction.normalized * movement.data.maxSpeed;
         Vector3 steer = desired - movement.Velocity;
-        Vector3 force = Vector3.ClampMagnitude(steer, movement.maxForce);
+        Vector3 force = Vector3.ClampMagnitude(steer, movement.data.maxForce);
 
         return force;
     }
